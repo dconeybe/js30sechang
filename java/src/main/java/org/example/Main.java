e34429a99ac1b5551e1fb300578eb11d4f7c3d3c
Denver Coneybeare <dconeybe@google.com>
2023-07-13 14:52:55 -0400

    added test 2, that loads from /etc/resolv.conf

--- java/src/main/java/org/example/Main.java
+++ java/src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,6 +31,7 @@ public final class Main {
       logger.log(Level.INFO, "listDocuments() starting");
 
       test1();
+      test2();
 
       clients.listDocuments().forEach(
           docRef ->
@@ -57,11 +63,72 @@ public final class Main {
       InitialDirContext dirContext = new InitialDirContext(env);
       dirContext.getAttributes("dns:///_grpclb._tcp.firestore.googleapis.com", new String[]{"SRV"});
     } catch (Exception e) {
-      System.out.println("zzyzx1 EXCEPTION BEGIN");
+      System.err.println("zzyzx1 EXCEPTION BEGIN");
       e.printStackTrace();
-      System.out.println("zzyzx1 EXCEPTION END");
+      System.err.println("zzyzx1 EXCEPTION END");
     } finally {
       System.out.println("zzyzx1 END " + System.nanoTime());
     }
+  }
+
+  private static void test2() throws IOException {
+    ArrayList<String> nameServers = loadNameserversFromResolvConf();
+    for (String nameServer : nameServers) {
+      System.out.println("zzyzx2 START [" + nameServer + "] " + System.nanoTime());
+      try {
+        Hashtable<String, String> env = new Hashtable<>();
+        env.put("com.sun.jndi.ldap.connect.timeout", "5000");
+        env.put("com.sun.jndi.ldap.read.timeout", "5000");
+        InitialDirContext dirContext = new InitialDirContext(env);
+        System.out.println("zzyzx2 [" + nameServer + "] dirContext.getAttributes(\"dns://" + nameServer + "/_grpclb._tcp.firestore.googleapis.com\", new String[]{\"SRV\"})");
+        dirContext.getAttributes("dns://" + nameServer + "/_grpclb._tcp.firestore.googleapis.com", new String[]{"SRV"});
+      } catch (Exception e) {
+        System.err.println("zzyzx2 [" + nameServer + "] EXCEPTION BEGIN");
+        e.printStackTrace();
+        System.err.println("zzyzx2 [" + nameServer + "] EXCEPTION END");
+      } finally {
+        System.out.println("zzyzx2 [" + nameServer + "] END " + System.nanoTime());
+      }
+    }
+  }
+
+  private static ArrayList<String> loadNameserversFromResolvConf() throws IOException {
+    ArrayList<String> nameServers = new ArrayList<>();
+    System.out.println("zzyzx loading /etc/resolv.conf started");
+    try (BufferedReader reader = new BufferedReader(new FileReader("/etc/resolv.conf"))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        line = line.trim();
+        if (line.isEmpty())
+          continue;
+        if (line.charAt(0) == '#' || line.charAt(0) == ';')
+          continue;
+        if (!line.startsWith("nameserver"))
+          continue;
+        String value = line.substring("nameserver".length());
+        if (value.isEmpty())
+          continue;
+        if (value.charAt(0) != ' ' && value.charAt(0) != '\t')
+          continue;
+        StringTokenizer st = new StringTokenizer(value, " \t");
+        while (st.hasMoreTokens()) {
+          String val = st.nextToken();
+          if (val.charAt(0) == '#' || val.charAt(0) == ';') {
+            break;
+          }
+          if (val.indexOf(':') >= 0 &&
+              val.indexOf('.') < 0 && // skip for IPv4 literals with port
+              val.indexOf('[') < 0 &&
+              val.indexOf(']') < 0 ) {
+            // IPv6 literal, in non-BSD-style.
+            val = "[" + val + "]";
+          }
+          nameServers.add(val);
+        }
+      }
+    }
+    System.out.println("zzyzx loading /etc/resolv.conf done; loaded "
+        + nameServers.size() + " name servers: " + nameServers);
+    return nameServers;
   }
 }
